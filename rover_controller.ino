2cbe9f202ad8b6bbb453a9879d0f6a0df6b2da4c
Kishan Grewal <kishangrewal06@gmail.com>
2025-06-02 14:59:44 +0100

    compiles

--- rover_controller.ino
+++ rover_controller.ino
@@ -5,6 +5,10 @@
 MotoronI2C mc1(0x10);
 MotoronI2C mc2(0x0B);
 
+const int16_t MOTOR_SPEED_MIN = 200;
+const int16_t MOTOR_SPEED_MAX = 800;
+const unsigned long LOOP_INTERVAL = 10;
+
 #define BUTTON_PIN 52
 #define DEBOUNCE_TIME 25
 int last_steady_state = LOW;
@@ -14,12 +18,12 @@ bool robot_enabled = false;
 unsigned long last_debounce_time = 0;
 
 void setDrive(float left_speed, float right_speed) {
-    left_speed = constrain(left_speed, MOTOR_SPEED_MIN, MOTOR_SPEED_MAX);
-    right_speed = constrain(right_speed, MOTOR_SPEED_MIN, MOTOR_SPEED_MAX);
-    int16_t left = (int16_t)round(left_speed);
-    int16_t right = (int16_t)round(right_speed);
-    mc1.setSpeed(2, -left);
-    mc1.setSpeed(3, right);
+  left_speed = constrain(left_speed, MOTOR_SPEED_MIN, MOTOR_SPEED_MAX);
+  right_speed = constrain(right_speed, MOTOR_SPEED_MIN, MOTOR_SPEED_MAX);
+  int16_t left = (int16_t)round(left_speed);
+  int16_t right = (int16_t)round(right_speed);
+  mc1.setSpeed(2, -left);
+  mc1.setSpeed(3, right);
 }
 
 void setup() {
@@ -60,28 +64,35 @@ void loop() {
     static unsigned long lastLoopTime = 0;
     unsigned long currentTime = millis();
 
-    // --- BUTTON HANDLING ---
-    current_state = digitalRead(BUTTON_PIN);
-    if (current_state != last_flickerable_state) {
+    if (currentTime - lastLoopTime >= LOOP_INTERVAL) {
+      lastLoopTime = currentTime;
+
+      setDrive(800.0, 800.0);
+
+      // --- BUTTON HANDLING ---
+      current_state = digitalRead(BUTTON_PIN);
+      if (current_state != last_flickerable_state) {
         last_debounce_time = millis();
         last_flickerable_state = current_state;
-    }
-    if ((millis() - last_debounce_time) > DEBOUNCE_TIME) {
+      }
+      if ((millis() - last_debounce_time) > DEBOUNCE_TIME) {
         if (last_steady_state == HIGH && current_state == LOW) {
             // Button press detected
         } else if (last_steady_state == LOW && current_state == HIGH) {
             robot_enabled = !robot_enabled;
         }
         last_steady_state = current_state;
-    }
+      }
 
-    if (currentTime - lastLoopTime >= LOOP_INTERVAL) {
-        lastLoopTime = currentTime;
-
-        setDrive(800.0, 800.0);
-
-        if (robot_enabled) == false) {
-          setDrive(0.0, 0.0);
-        }
+      // bool stop = handleWiFi(); // UDP logic
+      // if (stop == true) {
+      //   robot_enabled = false;
+      // }
+      if (robot_enabled == false) {
+        setDrive(0.0, 0.0);
+        mc2.setSpeed(2, 0.0);
+        mc2.setSpeed(3, 0.0);
+        // pid.reset();
+      }
     }
 }
